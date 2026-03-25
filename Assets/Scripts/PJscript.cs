d2a891ae091b553a1be5cffec478d3919ba591ac
martindroguett <martin.droguett@alumnos.ucn.cl>
2026-03-24 23:10:15 -0300

    Lista barra de vida y energía, además de coin para futuro inventario de monedas

--- Assets/Scripts/PJscript.cs
+++ Assets/Scripts/PJscript.cs
@@ -1,28 +1,54 @@
 using System;
+using System.Data;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerCtrl : MonoBehaviour
 {
+    //Atributos: 
     public float velocidadMov = 10;
-    private Vector2 direccionMov;
+    public int maxHealth = 100;
+    public int currentHealth;
+    public int maxEnergy = 100;
+    public int currentEnergy;
 
+    //Referencias:
+    private Vector2 direccionMov;
     public InputActionReference mover;
     public Rigidbody2D entidad;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public HealthBar healthBar;
+    public EnergyBar energyBar;
+
+    int count = 0;
+  
     void Start()
     {
         entidad = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
+        healthBar.setMaxHealth(maxHealth);
+        currentEnergy = maxEnergy;
+        energyBar.setMaxEnergy(maxEnergy);
     }
 
     // Update is called once per frame
     void Update()
     {
         direccionMov = mover.action.ReadValue<Vector2>();
+
+        count++;
+
+        if (count % 1000 == 0)
+        {
+            currentHealth -= 1;
+            healthBar.setHealth(currentHealth);
+        }
     }
 
     void FixedUpdate()
     {
         entidad.linearVelocity = new Vector2(direccionMov.x * velocidadMov,direccionMov.y * velocidadMov);
+
     }
 }
