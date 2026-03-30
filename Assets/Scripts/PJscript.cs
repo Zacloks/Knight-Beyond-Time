6fabd69b2ed11388427d528b613c39c7693c9e43
martindroguett <martin.droguett@alumnos.ucn.cl>
2026-03-30 14:09:49 -0300

    some act files
    act

--- Assets/Scripts/PJscript.cs
+++ Assets/Scripts/PJscript.cs
@@ -18,7 +18,8 @@ public class PlayerCtrl : MonoBehaviour
 
     //Referencias:
     private Vector2 direccionMov;
-    public InputActionReference mover;
+    public PlayerInput playerInput;
+    public Weapon equippedWeapon; 
     public Rigidbody2D entidad;
 
     public HealthBar healthBar;
@@ -29,6 +30,7 @@ public class PlayerCtrl : MonoBehaviour
   
     void Start()
     {
+        playerInput = GetComponent<PlayerInput>();
         entidad = GetComponent<Rigidbody2D>();
         currentHealth = maxHealth;
         healthBar.setMaxHealth(maxHealth);
@@ -40,8 +42,7 @@ public class PlayerCtrl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        direccionMov = mover.action.ReadValue<Vector2>();
-
+        direccionMov = playerInput.actions["MOVE"].ReadValue<Vector2>();
         count++;
 
         if (count % 1000 == 0)
@@ -57,6 +58,41 @@ public class PlayerCtrl : MonoBehaviour
     void FixedUpdate()
     {
         entidad.linearVelocity = new Vector2(direccionMov.x * velocidadMov,direccionMov.y * velocidadMov);
+    }
+  
+    public void Attack()
+    {
+        if (equippedWeapon == null) 
+        {
+            Debug.LogWarning("¡No tienes un arma equipada!");
+            return;
+        }
 
+        equippedWeapon.Atacar();
+        
+        Debug.Log("Atacando con " + equippedWeapon.weaponName);
+    }
+
+    public void MagicAttack()
+    {
+        if (equippedWeapon == null) 
+        {
+            Debug.LogWarning("¡No tienes un arma equipada!");
+            return;
+        }
+
+        equippedWeapon.Atacar();
+        
+        Debug.Log("Ataque mágico con " + equippedWeapon.weaponName);
+    }
+
+    public void Drop()
+    {
+        if (equippedWeapon == null) 
+        {
+            Debug.LogWarning("¡No tienes un arma equipada!");
+            return;
+        }
+        equippedWeapon=null;
     }
 }
