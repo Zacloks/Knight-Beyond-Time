204dd8c2f061dcc2557c7b777c65c92ef2ba4cd6
martindroguett <martin.droguett@alumnos.ucn.cl>
2026-04-15 23:41:53 -0400

    Correccion errores

---
+++ Assets/Scripts/Items/Consumible.cs
@@ -0,0 +1,21 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using System.Collections;
+
+public class Consumible : MonoBehaviour
+{
+    [Header("Atributos")]
+    public string name;
+    
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
